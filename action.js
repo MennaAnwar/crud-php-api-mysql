6dad4f5435aa59f69fccead0207f2cd415b9bb9c
Menna Mohamed <79084467+MennaAnwar@users.noreply.github.com>
2023-09-23 12:59:50 +0300

    Update action.js

--- action.js
+++ action.js
@@ -62,13 +62,29 @@ function editField(element, id) {
 }
 
 function Delete_User(id) {
-  $.ajax({
-    url: "submit.php",
-    method: "POST",
-    data: { id: id },
-    success: function (data) {
-      location.reload();
-    },
-    error: {},
+  Swal.fire({
+    title: "Are you sure?",
+    text: "You won't be able to revert this!",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Yes, delete it!",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      $.ajax({
+        url: "submit.php",
+        method: "POST",
+        data: { id: id },
+        success: function (data) {
+          Swal.fire("Deleted!", "Your file has been deleted.", "success").then(
+            () => {
+              location.reload();
+            }
+          );
+        },
+        error: {},
+      });
+    }
   });
 }
